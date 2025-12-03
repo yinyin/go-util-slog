d6dd8f5c5fa0b51165964c71871f931228e1f24d
Yinyin L <yinyinl@gmail.com>
2025-12-03 12:26:26 +0800

    refactor: extract log handling

--- error.go
+++ error.go
@@ -3,27 +3,11 @@ package utilslog
 import (
 	"context"
 	"log/slog"
-	"runtime"
-	"time"
 )
 
 // ErrorContext logs an error message with context.
 // The `err` parameter is REQUIRED, it will be included in the log attributes with "error" as its key.
 // Use the `.ErrorContext()` in original slog package if you don't want to include an error.
 func ErrorContext(ctx context.Context, msg string, err error, attrs ...slog.Attr) {
-	logger := slog.Default()
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	if !logger.Enabled(ctx, slog.LevelError) {
-		return
-	}
-	var pcs [1]uintptr
-	runtime.Callers(2, pcs[:]) // skip [Callers, ErrorContext]
-	r := slog.NewRecord(time.Now(), slog.LevelError, msg, pcs[0])
-	attrs = append(attrs, slog.Any("error", err))
-	if len(attrs) > 0 {
-		r.AddAttrs(attrs...)
-	}
-	_ = logger.Handler().Handle(ctx, r)
+	logErrAttrs(ctx, slog.LevelError, msg, err, attrs...)
 }
--- warn.go
+++ warn.go
@@ -3,27 +3,11 @@ package utilslog
 import (
 	"context"
 	"log/slog"
-	"runtime"
-	"time"
 )
 
 // WarnContext logs a warning message with context.
 // The `err` parameter is REQUIRED, it will be included in the log attributes with "error" as its key.
 // Use the `.WarnContext()` in original slog package if you don't want to include an error.
 func WarnContext(ctx context.Context, msg string, err error, attrs ...slog.Attr) {
-	logger := slog.Default()
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	if !logger.Enabled(ctx, slog.LevelWarn) {
-		return
-	}
-	var pcs [1]uintptr
-	runtime.Callers(2, pcs[:]) // skip [Callers, WarnContext]
-	r := slog.NewRecord(time.Now(), slog.LevelWarn, msg, pcs[0])
-	attrs = append(attrs, slog.Any("error", err))
-	if len(attrs) > 0 {
-		r.AddAttrs(attrs...)
-	}
-	_ = logger.Handler().Handle(ctx, r)
+	logErrAttrs(ctx, slog.LevelWarn, msg, err, attrs...)
 }
